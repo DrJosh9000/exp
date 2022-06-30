289b196a945dc8fe79f3309165afa804b76e1c9a
Josh Deprez <josh.deprez@gmail.com>
2022-06-30 01:57:24 +0000

    Add MustForEachLineIn helper

---
+++ load.go
@@ -0,0 +1,25 @@
+package exp
+
+import (
+	"bufio"
+	"log"
+	"os"
+)
+
+// MustForEachLineIn calls cb with each line in the file.
+// It uses a bufio.Scanner internally, which can fail on longer lines.
+// If an error is encountered, it calls log.Fatal.
+func MustForEachLineIn(path string, cb func(line string)) {
+	f, err := os.Open(path)
+	if err != nil {
+		log.Fatalf("MustForEachLineIn opening file: %v", err)
+	}
+	defer f.Close()
+	sc := bufio.NewScanner(f)
+	for sc.Scan() {
+		cb(sc.Text())
+	}
+	if err := sc.Err(); err != nil {
+		log.Fatalf("MustForEachLineIn: scanner: %v", err)
+	}
+}
