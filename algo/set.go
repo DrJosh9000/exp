70b0a3abe8d5273cdc927ca68c6bd0c2ed826d13
Josh Deprez <josh.deprez@gmail.com>
2022-08-31 13:46:46 +1000

    Set

---
+++ algo/set.go
@@ -0,0 +1,131 @@
+/*
+   Copyright 2022 Josh Deprez
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+	   http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+package algo
+
+// Set is a generic set type based on map. 
+// There's a million of these now; what harm is another?
+type Set[T comparable] map[T]struct{}
+
+// SetFromSlice creates a set from a slice.
+func SetFromSlice[T comparable](sl []T) Set[T] {
+	s := make(Set[T], len(sl))
+	s.Insert(sl...)
+	return s
+}
+
+// ToSlice returns a new slice with all the elements of the set.
+func (s Set[T]) ToSlice() []T {
+	sl := make([]T, 0, len(s))
+	for x := range s {
+		sl = append(sl, x)
+	}
+	return sl
+}
+
+// Insert inserts x into the set.
+func (s Set[T]) Insert(x ...T) {
+	for _, x := range x {
+		s[x] = struct{}{}
+	}
+}
+
+// make(Set[T]), delete(s, x), and len(s) are so simple that I'm not making methods.
+
+// Contains reports whether s contains x.
+func (s Set[T]) Contains(x T) bool {
+	_, c := s[x]
+	return c
+}
+
+// Add adds the elements from t into s.
+func (s Set[T]) Add(t Set[T]) {
+	for x := range t {
+		s.Insert(x)
+	}
+}
+
+// Subtract removes all elements in t from s.
+func (s Set[T]) Subtract(t Set[T]) {
+	for x := range t {
+		delete(s, x)
+	}
+}
+
+// Copy returns a copy of the set.
+func (s Set[T]) Copy() Set[T] {
+	t := make(Set[T], len(s))
+	t.Add(s)
+	return t
+}
+
+// Union returns a new set containing elements from both sets.
+func (s Set[T]) Union(t Set[T]) Set[T] {
+	u := make(Set[T], len(s) + len(t))
+	u.Add(s)
+	u.Add(t)
+	return u
+}
+
+// Intersection returns a new set with elements common to both sets. 
+func (s Set[T]) Intersection(t Set[T]) Set[T] {
+	u := make(Set[T])
+	for x := range s {
+		if t.Contains(x) {
+			u.Insert(x)
+		}
+	}
+	return u
+}
+
+// Difference returns a new set with elements from s that are not in t.
+func (s Set[T]) Difference(t Set[T]) Set[T] {
+	u := s.Copy()
+	u.Subtract(t)
+	return u
+}
+
+// SymmetricDifference returns a new set with elements that are either in 
+// s or in t but not both.
+func (s Set[T]) SymmetricDifference(t Set[T]) Set[T] {
+	u := make(Set[T])
+	for x := range s {
+		if !t.Contains(x) {
+			u.Insert(x)
+		}
+	}
+	for x := range t {
+		if !s.Contains(x) {
+			u.Insert(x)
+		}
+	}
+	return u
+}
+
+// SubsetOf reports whether s is a subset or equal to t.
+func (s Set[T]) SubsetOf(t Set[T]) bool {
+	for x := range s {
+		if !t.Contains(x) {
+			return false
+		}
+	}
+	return true
+}
+
+// Equal reports whether two sets are equal.
+func (s Set[T]) Equal(t Set[T]) bool {
+	return s.SubsetOf(t) && t.SubsetOf(s)
+}
--- algo/unionfind.go
+++ algo/unionfind.go
@@ -5,7 +5,7 @@
    you may not use this file except in compliance with the License.
    You may obtain a copy of the License at
 
-	   http://www.apache.org/licenses/LICENSE-2.0
+       http://www.apache.org/licenses/LICENSE-2.0
 
    Unless required by applicable law or agreed to in writing, software
    distributed under the License is distributed on an "AS IS" BASIS,
@@ -19,34 +19,54 @@ package algo
 import "math/rand"
 
 // DisjointSets implements union-find algorithms for disjoint sets.
-type DisjointSets[K comparable] map[K]K
+type DisjointSets[T comparable] map[T]T
 
 // Find returns the representative element of the set containing x.
 // It freely modifies d.
 // If x is not contained in d, Find inserts x as a new disjoint singleton set
 // within d and returns x.
-func (d DisjointSets[K]) Find(x K) K {
-   if _, found := d[x]; !found {
-      d[x] = x
-      return x
-   }
-   for x != d[x] {
-      d[x] = d[d[x]] // path compression
-      x = d[x]
-   }
-   return x
+func (d DisjointSets[T]) Find(x T) T {
+ 	if _, found := d[x]; !found {
+ 		d[x] = x
+ 		return x
+ 	}
+ 	for x != d[x] {
+ 		d[x] = d[d[x]] // path compression
+ 		x = d[x]
+ 	}
+ 	return x
 }
 
 // Union merges the set containing x with the set containing y.
 // If both of the elements are not contained in d, a new set is created.
-func (d DisjointSets[K]) Union(x, y K) {
-   p, q := d.Find(x), d.Find(y)
-   if p == q {
-      return
+func (d DisjointSets[T]) Union(x, y T) {
+ 	p, q := d.Find(x), d.Find(y)
+ 	if p == q {
+ 		return
+ 	}
+ 	if rand.Intn(2) == 0 {
+ 		d[p] = q
+ 	} else {
+ 		d[q] = p
+ 	}
+}
+
+// Reps returns a set containing each representative element.
+func (d DisjointSets[T]) Reps() Set[T] {
+ 	s := make(Set[T])
+ 	for x := range d {
+ 		s.Insert(d.Find(x))
+ 	}
+ 	return s
+}
+
+// Sets returns all the sets in d, in a map from the representative element
+// to a slice containing all members of that set.
+func (d DisjointSets[T]) Sets() map[T][]T {
+   m := make(map[T][]T)
+   for x := range d {
+      r := d.Find(x)
+      m[r] = append(m[r], x)
    }
-   if rand.Intn(2) == 0 {
-      d[p] = q
-   } else {
-      d[q] = p
-   }
+   return m
 }
