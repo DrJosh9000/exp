d4d22c0631847e8615d84a7a1ea3d802250becff
Josh Deprez <josh.deprez@gmail.com>
2023-05-25 14:16:05 +1000

    Actually don't

--- .buildkite/all_tests.sh
+++ .buildkite/all_tests.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -euo pipefail
 
-buildkite-agent lock acquire LLAMA
-trap "buildkite-agent lock release LLAMA" EXIT
-
 go install gotest.tools/gotestsum@latest
 gotestsum --junitfile junit.xml ./...
 
