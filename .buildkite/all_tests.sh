f01387ca1e65536a92f22e65d84fb9c183057ee5
Josh Deprez <josh.deprez@gmail.com>
2023-05-25 14:15:27 +1000

    Let's try locks

--- .buildkite/all_tests.sh
+++ .buildkite/all_tests.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 set -euo pipefail
+
+buildkite-agent lock acquire LLAMA
+trap "buildkite-agent lock release LLAMA" EXIT
+
 go install gotest.tools/gotestsum@latest
 gotestsum --junitfile junit.xml ./...
+
