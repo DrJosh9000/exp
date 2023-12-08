68536639e337c381664e060460a9adc11abd3f06
Josh Deprez <josh.deprez@gmail.com>
2023-12-08 15:13:44 +1100

    para.Reduce

--- para/para.go
+++ para/para.go
@@ -4,14 +4,16 @@ package para
 import (
 	"runtime"
 	"sync"
+
+	"github.com/DrJosh9000/exp/algo"
 )
 
 // Map calls f with each element of in, to build the output slice.
-// It does the mapping in parallel with GOMAXPROCS goroutines.
+// It does the mapping in parallel, using GOMAXPROCS goroutines.
 func Map[S ~[]X, X, Y any](in S, f func(X) Y) []Y {
-	out := make([]Y, len(in))
-
 	N := runtime.GOMAXPROCS(0)
+
+	out := make([]Y, len(in))
 	var wg sync.WaitGroup
 	wg.Add(N)
 	for i := 0; i < N; i++ {
@@ -27,4 +29,26 @@ func Map[S ~[]X, X, Y any](in S, f func(X) Y) []Y {
 
 	wg.Wait()
 	return out
+}
+
+// Reduce reduces a slice to a single value.
+// It does the mapping in parallel, using GOMAXPROCS goroutines.
+func Reduce[S ~[]E, E any](in S, f func(E, E) E) E {
+	N := runtime.GOMAXPROCS(0)
+	out := make([]E, N)
+	var wg sync.WaitGroup
+	wg.Add(N)
+	for i := 0; i < N; i++ {
+		i := i
+		go func() {
+			defer wg.Done()
+
+			for j := i; j < len(in); j += N {
+				out[i] = f(out[i], in[j])
+			}
+		}()
+	}
+
+	wg.Wait()
+	return algo.Foldl(out, f)
 }
